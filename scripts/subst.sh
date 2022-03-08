fc1230e2c3bf4615f50ec91566a0f324aded1990
Karl Kaiser <karl@kwkaiser.io>
2022-03-08 12:53:12 -0500

    Various tweaks & configuration improvements

--- scripts/subst.sh
+++ scripts/subst.sh
@@ -4,8 +4,8 @@ cd $(dirname "$0")
 
 function sed-parts () {
     # Substitute in nav
-    local nav=$(cat ../src/subst/nav.html)
-    local filled=$(perl -p -e "s#{replace-nav}#${nav}#g" ${1})
+    local sidebar=$(cat ../src/subst/sidebar.html)
+    local filled=$(perl -p -e "s#{replace-sidebar}#${sidebar}#g" ${1})
     echo ${filled} > ${1}
 
     # Substitute in header
